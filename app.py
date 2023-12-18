3b5d23d74674cdc9ec68f075205fd9c382ba7bdf
Kukkis66 <matti.kukkasjarvi@gmail.com>
2023-12-18 10:23:26 +0200

    added total cost

--- app.py
+++ app.py
@@ -3,16 +3,22 @@ from flask_cors import CORS
 import json
 
 
-
+import os
 app = Flask(__name__)
 CORS(app)
 
-JSON_FILE = "shellyReadings.json"
+JSON_FILE = os.path.join(os.path.dirname(__file__), 'shelly.json')
 
 def read_json_file():
     with open(JSON_FILE, 'r') as file:
         data = json.load(file)
     return data
+
+
+def read_settings():
+    with open('settings.json', 'r') as file:
+        settings = json.load(file)
+        return settings
 
 @app.route('/api', methods=['GET'])
 def get_data():
--- shelly.py
+++ shelly.py
@@ -5,6 +5,11 @@ import os
 import schedule
 import time
 import requests
+
+
+
+
+
 class Shelly:
     
     
@@ -58,9 +63,11 @@ class Shelly:
             last_entry = json_data[-1]
             last_timestamp = last_entry.get('time')
             last_value = float(last_entry.get('total_watts'))
+            last_cost = float(last_entry.get('total_cost'))
         else:
             last_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            last_value = 0.0  # Assuming a default value if the file is empty
+            last_value = current_value  # Assuming a default value if the file is empty
+            last_cost = 0.0 # Assuming a default if the file is empty
 
         if last_timestamp:
             last_time = datetime.strptime(last_timestamp, "%Y-%m-%d %H:%M:%S")
@@ -81,6 +88,10 @@ class Shelly:
             cost = (subtraction_result/1000) * price
         else:
             cost = 0.0
+
+        #calculate total cost
+        
+        total_cost =+ last_cost + cost
         # Add the current timestamp and value to the JSON data
         current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         new_entry = {
@@ -88,7 +99,8 @@ class Shelly:
             'total_watts': current_value,
             'watts_during_time_interval': round(subtraction_result, 3),
             'time_interval': round(time_difference),
-            'price_during_time_interval': round(cost, 2)
+            'price_during_time_interval': round(cost, 3),
+            'total_cost': round(total_cost, 3)
         }
         json_data.append(new_entry)
         print(new_entry)
@@ -120,7 +132,7 @@ class Shelly:
             time.sleep(1)
             self.get_price()
         
-        schedule.every().minute.at(":00").do(wrapper)
+        schedule.every().hour.at(":00").do(wrapper)
         # Keep the program running
         while True:
             schedule.run_pending()
